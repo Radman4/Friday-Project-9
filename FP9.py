9162a9e6273881bee39df613d63f55ad56de64d2
Radman4 <nootnoot432@gmail.com>
2025-04-16 10:06:32 -0500

    changed the env files to just .env for readability added the basic skeleton to the gui. this should in theory call the openai module to be used when the button is submitted. will work on changing the current prompt to actually being what the self.promptText says.

--- FP9.py
+++ FP9.py
@@ -1,8 +1,8 @@
 import tkinter as ttk
 import openai
-import dotenv
+import dotenv 
 import os
-apikey = os.getenv("personalkey.env")
+apikey = os.getenv("personalkey")
 #setup openai (will need to prchase tokens, i think)
 client = openai.OpenAI(
     api_key=apikey
@@ -17,4 +17,21 @@ class prompt:
     #textbox for the prompt
         self.promptText = ttk.Label(frame1, text ="Enter your prompt here")
         self.prompt = ttk.Entry(frame1)
-        
+        self.button1 = ttk.Button(frame1, text="Submit Prompt")
+        self.button1.config(command=self.promptAnswer)
+        #now to get ChatGPT to actually use the prompt!
+    def promptAnswer(self):
+        client = openai.OpenAI(
+        api_key=apikey
+    )
+    response = client.responses.create(
+    model="gpt-4.1",
+    input=[
+        {"role": "user", "content": "what teams are playing in this image?"},
+        {"role": "user","content": [{"type": "input_text",
+                    "image_url": "https://upload.wikimedia.org/wikipedia/commons/3/3b/LeBron_James_Layup_%28Cleveland_vs_Brooklyn_2018%29.jpg"}]}])
+
+    print(response.output_text)
+
+
+    
