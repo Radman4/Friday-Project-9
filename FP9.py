e1cda6c98a295a45192eae96990d6208f92a5c08
Radman4 <nootnoot432@gmail.com>
2025-04-18 12:34:49 -0500

    main window now shows up properly. text can be entered, a button can be pressed, but currently it thorws a error connecting to the chatGPT prompt. said prompt seems ready to be used as is. will check later on if it needs more work to be done.

--- FP9.py
+++ FP9.py
@@ -1,37 +1,49 @@
-import tkinter as ttk
-import openai
-import dotenv 
-import os
-apikey = os.getenv("personalkey")
-#setup openai (will need to prchase tokens, i think)
-client = openai.OpenAI(
-    api_key=apikey
-)
 
-promptWindow = ttk()
+from tkinter import *
+from tkinter import ttk
+import openai
+from dotenv import load_dotenv
+import os
+apikey = os.getenv("key")
+#setup openai (will need to purchase tokens, i think)
+openai.api_key = apikey
+root = Tk()
 
 class prompt:
     def __init__(self, master):
         frame1 = ttk.Frame(master, height = 200, width=300)
-        frame1.pack()
+        frame1.pack(padx=10, pady=10)
     #textbox for the prompt
         self.promptText = ttk.Label(frame1, text ="Enter your prompt here")
+        self.promptText.grid(column= 0, row= 0)
         self.prompt = ttk.Entry(frame1)
+        self.prompt.grid(column= 0, row= 1)
         self.button1 = ttk.Button(frame1, text="Submit Prompt")
         self.button1.config(command=self.promptAnswer)
+        self.button1.grid(column= 0, row= 2)
         #now to get ChatGPT to actually use the prompt!
     def promptAnswer(self):
-        client = openai.OpenAI(
-        api_key=apikey
-    )
-    response = client.responses.create(
-    model="gpt-4.1",
-    input=[
-        {"role": "user", "content": "what teams are playing in this image?"},
-        {"role": "user","content": [{"type": "input_text",
-                    "image_url": "https://upload.wikimedia.org/wikipedia/commons/3/3b/LeBron_James_Layup_%28Cleveland_vs_Brooklyn_2018%29.jpg"}]}])
+            #  new window for output
+        outputText = ttk.Frame(outputWindow, height=15, width=60)
+        outputText.pack(padx=10, pady=10)
+        outputWindow = ttk.Label(outputText, answer)
 
-    print(response.output_text)
+        try:
+            # writing to chat GPT to tell it that  
+            response = openai.ChatCompletion.create(
+                model="gpt-4.1",  
+                messages=[
+                    {"role": "system", "content": "You are a helpful assistant."},
+                    {"role": "user", "content": prompt},])
+        # Extract ChatGPT's response
+            answer = response.choices[0].message.content.strip()
+        except Exception as e:
+            answer = f"An error occurred: {e}"
 
+
+    
+
+app = prompt(root)
+root.mainloop()
 
     
